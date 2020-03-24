5757a68d3aa312d38c009836a24527aab2c8fe44
Amber Mohler <ambermohler@iMac.local>
2020-03-24 19:16:04 -0400

    completed restricted-middleware.js and added restricted route to server.js, tested and everything is working with proper jwt authorization token. completed mvp

--- api/server.js
+++ api/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
-const server = express();
-
 const usersRouter = require("../users/users-router");
 const authRouter = require("../auth//auth-router");
+const restricted = require("../auth/restricted-middleware");
+
+const server = express();
 
 server.use(express.json());
 
-server.use("/api/users", usersRouter);
+server.use("/api/users", restricted, usersRouter);
 server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -28,9 +28,9 @@ router.post("/login", (req, res) => {
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
 
-        res.status(200).json({ message: `Welcome, ${user.username}!` });
+        res.status(200).json({ message: `Welcome, ${user.username}!`, token });
       } else {
-        res.status(401).json({ message: "Invalid Credentials" });
+        res.status(401).json({ message: "You shall not pass!" });
       }
     })
     .catch(error => {
