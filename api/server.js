dcb44f2a35d19c235618dd3f3435e47fd1bfc24a
Amber Mohler <ambermohler@iMac.local>
2020-03-24 18:58:53 -0400

    created register and login endpoints and tested to make sure they work, created a generateToken variable to create a token on login, created a config folder with secrets file to hold jwt secret

--- api/server.js
+++ api/server.js
@@ -3,10 +3,12 @@ const express = require("express");
 const server = express();
 
 const usersRouter = require("../users/users-router");
+const authRouter = require("../auth//auth-router");
 
 server.use(express.json());
 
 server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "Working!" });
